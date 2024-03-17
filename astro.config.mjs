6ccaca5b3923a81682883f714a8a476be8f6a217
Brayan M. Beltre <brayanmbeltre@gmail.com>
2024-03-16 20:35:23 -0400

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  }), sentry(), spotlightjs(), db()]
+});
